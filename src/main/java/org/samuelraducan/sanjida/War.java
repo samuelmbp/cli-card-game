cc7538600eedf15c8819b65a4807891801d87cf7
Sanjida <sanji_b@outlook.com>
2024-05-05 12:31:59 +0100

    adding card logic

--- src/main/java/org/samuelraducan/sanjida/War.java
+++ src/main/java/org/samuelraducan/sanjida/War.java
@@ -1,10 +1,8 @@
 package org.samuelraducan.sanjida;
 
-import org.samuelraducan.Deck;
-import org.samuelraducan.Game;
-import org.samuelraducan.GameConsole;
-import org.samuelraducan.Player;
+import org.samuelraducan.*;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class War extends Game {
@@ -17,29 +15,51 @@ public class War extends Game {
     public War(String title, String rules) {
         super(title, rules);
         this.scanner = new Scanner(System.in);
-        this.gameConsole = new GameConsole();
+        this.gameConsole = new GameConsole();  // initialises the game console
+        this.deckOfCards = new Deck();
     }
 
     public void startGame() {
-        gameConsole.welcomeMessage("War");
-        System.out.println("Please press 1 to play, and 2 to see the rules");
        String usersInput = scanner.nextLine();
         if (usersInput.equals("1")) {
-            play();
         } else if (usersInput.equals("2")) {
             printRules();
             System.out.println();
+            System.out.println("Please press 1 to play, or 2 to see the rules");
             startGame();
         } else {
             System.out.println("Invalid input. Please press 1 or 2");
             System.out.println();
+            System.out.println("Please press 1 to play, or 2 to see the rules");
             startGame();
         }
     }
 
+
+//    public void
+
+//    deck1.addAll(deckOfCards.sublist())
     @Override
     public void play() {
+        gameConsole.welcomeMessage("War");
+        System.out.println("Please press 1 to play, or 2 to see the rules");
+        startGame();
 
+        deckOfCards.shuffleDeck();
+        LinkedList<Card>deck1 = new LinkedList<>();
+        LinkedList<Card>deck2 = new LinkedList<>();
+        deck1.addAll(deckOfCards.getDeckOfCards().subList(0, 26));              //26 cards for p1
+        deck2.addAll(deckOfCards.getDeckOfCards().subList(26, deckOfCards.getDeckOfCards().size()));
+//        System.out.println(deck1);
+//        System.out.println(deck2);
+//        System.out.println(deck1.stream().distinct().count());
+//        System.out.println(deck2.stream().distinct().count());
+
+        Card player1Card = deck1.pop();
+        Card player2Card = deck2.pop();
+        System.out.println(player1Card.toString());
+        System.out.println(player2Card.toString());
+        System.out.println(deck2.stream().distinct().count());
         // add override logic here
     }
 
